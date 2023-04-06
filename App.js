bd8f57a2abc2a7ac1c1abcaf4d15454ecbf5ee70
victorlabos <labosvictor1@gmail.com>
2023-04-05 22:30:57 -0300

    primeiro APP

--- App.js
+++ App.js
@@ -4,8 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <View style={styles.titulo} >olá mundo</View>
+
+      <text>
+      <h2>Oi meu nome é Victor e esse é meu primeiro AAP no React </h2>
+      </text>
+
     </View>
   );
 }
@@ -13,8 +17,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ccc',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  titulo:{
+    backgroundColor: '#483D8B',	
+    padding: '12',
+    borderBottomLeftRadius: '6',
+    fontSize: 100
+  }
+
+
 });
